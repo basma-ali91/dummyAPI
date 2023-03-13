abebf82801f3fc4c35225925e14738ccd8f0124d
basma-ali91 <basmaali01@gmail.com>
2023-03-13 15:03:25 +0200

    dummy

---
+++ js/main.js
@@ -0,0 +1,31 @@
+let products = document.getElementById("products");
+let result=[]
+async function getproducts(){
+    let apiurlproducts = await fetch("https://dummyjson.com/products");
+    let response = await apiurlproducts.json();
+    result= response.products;
+    console.log(response);
+    displayproducts()
+    
+}
+function displayproducts(){
+    let str=``;
+    for(let i=0 ;  i<result.length ;i++){
+        str +=
+        `<div class="col-md-4">
+         <div class="bg-info p-4 m-3">
+            <h3>${result[i].category}</h3>
+            <h5>${result[i].price}</h5>
+            <h3>${result[i].rating}</h3>
+            <h6 class="text-danger">discountPercentage : ${result[i].discountPercentage}</h6>
+            <div>
+            <img src="${result[i].thumbnail?result[i].thumbnail:'image/images.png'}" class="w-100">
+            </div>
+         </div>
+         </div>
+`
+}
+products.innerHTML=str;
+console.log(str)
+}
+getproducts();
