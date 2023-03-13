2a95e84e47292cefd6784fcd5b68c16478b3df42
basma-ali91 <basmaali01@gmail.com>
2023-03-13 16:06:22 +0200

    dummy

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 let products = document.getElementById("products");
+let search =document.getElementById("Search")
+
 let result=[]
 async function getproducts(){
     let apiurlproducts = await fetch("https://dummyjson.com/products");
     let response = await apiurlproducts.json();
     result= response.products;
-    console.log(response);
+    // console.log(response);
     displayproducts()
     
 }
@@ -15,8 +17,10 @@ function displayproducts(){
         `<div class="col-md-4">
          <div class="bg-info p-4 m-3">
             <h3>${result[i].category}</h3>
+            <h5>${result[i].title}</h5>
             <h5>${result[i].price}</h5>
             <h3>${result[i].rating}</h3>
+
             <h6 class="text-danger">discountPercentage : ${result[i].discountPercentage}</h6>
             <div>
             <img src="${result[i].thumbnail?result[i].thumbnail:'image/images.png'}" class="w-100">
@@ -26,6 +30,35 @@ function displayproducts(){
 `
 }
 products.innerHTML=str;
-console.log(str)
 }
 getproducts();
+
+
+function searchfunction(){
+
+    let searchword =``;
+    for(let i=0 ;  i<result.length ;i++){
+        if(result[i].title.toLowerCase().includes(search.value.toLowerCase())){
+            searchword +=
+            `<div class="col-md-4">
+             <div class="bg-info p-4 m-3">
+                <h3>${result[i].category}</h3>
+                <h5>${result[i].title}</h5>
+                <h5>${result[i].price}</h5>
+                <h3>${result[i].rating}</h3>
+                <h6 class="text-danger">discountPercentage : ${result[i].discountPercentage}</h6>
+                <div>
+                <img src="${result[i].thumbnail?result[i].thumbnail:'image/images.png'}" class="w-100">
+                </div>
+             </div>
+             </div>
+    `
+        }
+       
+}
+products.innerHTML=searchword;
+}
+search.addEventListener("keyup",function(){
+    console.log(search.value)
+        searchfunction()
+     })
